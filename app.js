674f637a84831d2be2c75ad307ad163b320cfa11
Shubham Raosaheb Kharde <shushwenira@gmail.com>
2020-02-04 14:06:13 -0600

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));;
 
 
 // configure passport
